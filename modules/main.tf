507d94db08c3e682e30e9b5198ec42d0876ebe12
Amr Farid <amr.farid222@gmail.com>
2017-09-01 02:27:58 +0200

    Creating ec2 instances for master and minions

---
+++ modules/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "ec2" {
+  ami = "${var.ami_id}"
+  vpc_security_group_ids = ["${split(",", var.security_group_id)}"]
+  key_name = "${var.key_name}"
+  instance_type = "${var.instance_type}"
+  count = "${var.count}" 
+  iam_instance_profile="${var.iam_instance_profile}"
+  user_data = "${var.user_data}"
+  subnet_id = "${element(split(",", var.subnet_id), count.index%2)}"
+  tags {
+    Name = "${var.name}"
+    server_role = "${var.server_role}"
+    sshUser = "ubuntu"
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 120"
+  }
+
+}
+output "ec2_id" {
+  value = "${join(",", aws_instance.ec2.*.id)}"
+}
